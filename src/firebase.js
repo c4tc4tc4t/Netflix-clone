ad06ae0fe9dc1187e3540b632912dc9cf8a7daa2
c4tc4tc4t <emilano.ito@gmail.com>
2022-07-29 12:11:03 -0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD90P0WZyTdzJ9os7o9RNoxTqelwqg6oY0",
+  authDomain: "netflix-clone-c71b3.firebaseapp.com",
+  projectId: "netflix-clone-c71b3",
+  storageBucket: "netflix-clone-c71b3.appspot.com",
+  messagingSenderId: "1029863482085",
+  appId: "1:1029863482085:web:450bc22d4cda05642aea0c",
+};
